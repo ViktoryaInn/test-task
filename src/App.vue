80dfc0f23db6d5e782e632ef396aadae35d272bd
Viktoria <vicmurrmiu@gmail.com>
2022-06-10 02:31:31 +0500

    feat(calculate): add styles for CalculateForm.vue and App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template lang="pug">
   div(id="app")
+    h3 Вычислите сумму двух наименьших чисел
+    p Введите нескоколько чисел черз запятую, чтобы вычислить сумму
     calculate-form(@calculate="result = $event", @beforeCalculate="result = null")
-    p(v-if="result !== null") Cумма двух минимальных чисел {{result}}
+    p(v-if="result !== null") Cумма двух минимальных чисел: {{result}}
 </template>
 
 <script>
--- src/components/CalculateForm.vue
+++ src/components/CalculateForm.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
   form(@keyup.enter.prevent="calculate()")
-    textarea(v-model="value")
-    error-message(:message="error")
-    button(@click.prevent="calculate()") biba
+    textarea.form-item(v-model="value")
+    error-message.form-item(:message="error")
+    button.form-item(@click.prevent="calculate()") Вычислить
 </template>
 
 <script>
@@ -36,6 +36,53 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="stylus" scoped>
+textarea
+  width 100%
+  max-width 320px
+  height 180px
+  padding 16px
+  border none
+  border-radius 15px
+  background-color white
+  box-shadow 0px 0px 5px rgba(0, 0, 0, 0.15)
+  resize none
+  font-family font-family Avenir, Helvetica, Arial, sans-serif
+  font-size 18px
+  color #2c3e50
 
+textarea:focus-visible
+  border none
+  outline none
+  box-shadow 0px 0px 5px rgba(0, 0, 0, 0.25)
+
+button
+  width 150px
+  height 45px
+  border none
+  border-radius 15px
+  background-color #42b983
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.15);
+  font-family font-family Avenir, Helvetica, Arial, sans-serif
+  font-size 18px
+  font-weight 600
+  color white
+  cursor pointer
+
+button:hover
+  background-color #3ca676
+  transition background-color 1s
+
+button:active
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  transition background-color 0,5s
+
+form
+  display flex
+  flex-direction column
+  align-items center
+
+.form-item
+  margin-bottom 16px
+  display block
 </style>
